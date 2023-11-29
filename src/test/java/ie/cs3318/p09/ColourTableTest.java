1edd6a76bd24821c71783ca1d508b6fe1f0b9b30
jnovarro <joseph.novarro@amorginservices.com>
2023-11-29 19:25:24 +0000

    - wrote assertDoesNotThrow for valid value or size -> test passed - wrote assertThrows for non-power-of-two size inputs, zero size, and for size greater than 1024 Added a method to  verify an added colour: - assertDoesNotThrow & assertEquals used to verify and match added Colour - passed Added test to pass if color is being added beyond the table size Added to test to pass if exception is raised when adding the same color more than once: - passed

--- src/test/java/ie/cs3318/p09/ColourTableTest.java
+++ src/test/java/ie/cs3318/p09/ColourTableTest.java
@@ -1,12 +1,16 @@
 package ie.cs3318.p09;
 
-
-
-
+import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class ColourTableTest {
+    private ColourTable table;
+
+    @BeforeEach
+    public void setUp() {
+        table = new ColourTable(8); // Initialize with a table size of 8
+    }
 
     @Test
     public void testValidColourTableSize() {
@@ -18,5 +22,24 @@ public class ColourTableTest {
         assertThrows(IllegalArgumentException.class, () -> new ColourTable(3), "Constructor should throw IllegalArgumentException for a non-power-of-two size");
         assertThrows(IllegalArgumentException.class, () -> new ColourTable(0), "Constructor should throw IllegalArgumentException for zero size");
         assertThrows(IllegalArgumentException.class, () -> new ColourTable(1025), "Constructor should throw IllegalArgumentException for size greater than 1024");
+    }
+
+    @Test
+    public void testAddingValidColour() {
+        assertDoesNotThrow(() -> table.add(0, 0xFFFFFF), "Should not throw an exception when adding a valid colour");
+        assertEquals(0xFFFFFF, table.getColour(0), "The colour should match what was added");
+    }
+
+    @Test
+    public void testAddingColourOutOfBounds() {
+        assertThrows(IndexOutOfBoundsException.class, () -> table.add(8, 0xFFFFFF), "Should throw IndexOutOfBoundsException when adding beyond table size");
+    }
+
+
+    @Test
+    public void testAddingSameColourTwice() {
+        int colour = 0xABCDEF;
+        table.add(0, colour);
+        assertThrows(IllegalArgumentException.class, () -> table.add(1, colour), "Should throw IllegalArgumentException when adding the same colour twice");
     }
 }
